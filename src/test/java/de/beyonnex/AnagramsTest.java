60eec8d0d438cc83fe09f19c14ddcb24881e3c07
= <=>
2023-08-02 15:19:21 +0200

    Initial commit

---
+++ src/test/java/de/beyonnex/AnagramsTest.java
@@ -0,0 +1,28 @@
+package de.beyonnex;
+
+import org.junit.Test;
+
+public class AnagramsTest {
+
+    private final Anagrams anagrams = new Anagrams();
+
+    @Test
+    public void shouldReturnFalseWhenEitherInputStringIsNull() {
+
+    }
+
+    @Test
+    public void shouldReturnTrueWhenInputStringAreAnagrams() {
+
+    }
+
+    @Test
+    public void shouldReturnFalseWhenInputStringAreNotAnagrams() {
+
+    }
+
+    @Test
+    public void shouldReturnFalseWhenInputStringsAreOfDifferentLengthMakingThemNonAnagrams() {
+
+    }
+}
