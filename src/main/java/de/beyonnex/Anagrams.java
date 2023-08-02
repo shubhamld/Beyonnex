ed42576f091248ca4cd41806f2d13926e0cb4247
= <=>
2023-08-02 15:42:44 +0200

    solution commit + Tests impl.

--- src/main/java/de/beyonnex/Anagrams.java
+++ src/main/java/de/beyonnex/Anagrams.java
@@ -1,4 +1,34 @@
 package de.beyonnex;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Anagrams {
+
+    public boolean isAnagrams(String input1, String input2) {
+        if ((input1 == null || input2 == null) || (input1.length() != input2.length())) {
+            return false;
+        }
+
+        Map<Character, Integer> charCountTable = new HashMap<>();
+
+        for (int i = 0; i < input1.length(); i++) {
+            charCountTable.put(
+                    input1.charAt(i), charCountTable.getOrDefault(input1.charAt(i), 0) + 1
+            );
+        }
+
+        for (int i = 0; i < input2.length(); i++) {
+            charCountTable.put(
+                    input2.charAt(i), charCountTable.getOrDefault(input2.charAt(i), 0) - 1
+            );
+        }
+
+        for (int val : charCountTable.values()) {
+            if (val != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- src/test/java/de/beyonnex/AnagramsTest.java
+++ src/test/java/de/beyonnex/AnagramsTest.java
@@ -2,27 +2,54 @@ package de.beyonnex;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 public class AnagramsTest {
 
     private final Anagrams anagrams = new Anagrams();
 
     @Test
     public void shouldReturnFalseWhenEitherInputStringIsNull() {
-
+        assertFalse(anagrams.isAnagrams("anagram", null));
+        assertFalse(anagrams.isAnagrams(null, null));
     }
 
     @Test
     public void shouldReturnTrueWhenInputStringAreAnagrams() {
-
+        assertTrue(anagrams.isAnagrams("anagram", "magaran"));
+        assertTrue(anagrams.isAnagrams("car", "arc"));
+        assertTrue(anagrams.isAnagrams("map", "pam"));
+        assertTrue(anagrams.isAnagrams("hannah", "hannah"));
     }
 
     @Test
     public void shouldReturnFalseWhenInputStringAreNotAnagrams() {
-
+        assertFalse(anagrams.isAnagrams("anagram", "togaran"));
     }
 
     @Test
     public void shouldReturnFalseWhenInputStringsAreOfDifferentLengthMakingThemNonAnagrams() {
+        assertFalse(anagrams.isAnagrams("anagram", "aanagramss"));
+    }
 
+    @Test
+    public void shouldReturnTrueWhenInputStringsWithDifferentCasesAreAnagrams() {
+        assertTrue(anagrams.isAnagrams("Anagram", "magarAn"));
+    }
+
+    @Test
+    public void shouldReturnFalseWhenInputStringsWithDifferentCasesAreNotAnagrams() {
+        assertFalse(anagrams.isAnagrams("Anagram", "magaran"));
+    }
+
+    @Test
+    public void shouldReturnTrueWhenInputStringsWithSpecialCharsAreAnagrams() {
+        assertTrue(anagrams.isAnagrams("anagram$", "$magaran"));
+    }
+
+    @Test
+    public void shouldReturnFalseWhenInputStringsWithSpecialCharsAreNotAnagrams() {
+        assertFalse(anagrams.isAnagrams("anagram$", "@magaran"));
     }
 }
